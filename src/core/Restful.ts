a22c58219c1b15692c6d37c04e5b565b8233e697
mmdzov <mzov939@gmail.com>
2022-12-12 17:41:40 +0330

    Support other request methods

--- src/core/Restful.ts
+++ src/core/Restful.ts
@@ -11,3 +11,51 @@ export const Get =
       target,
     });
   };
+
+export const Post =
+  (path?: string) =>
+  (target: any, propKey: string, descriptor: PropertyDescriptor) => {
+    restDecorHandler({
+      method: "POST",
+      propKey,
+      descriptor,
+      path,
+      target,
+    });
+  };
+
+export const Put =
+  (path?: string) =>
+  (target: any, propKey: string, descriptor: PropertyDescriptor) => {
+    restDecorHandler({
+      method: "PUT",
+      propKey,
+      descriptor,
+      path,
+      target,
+    });
+  };
+
+export const Patch =
+  (path?: string) =>
+  (target: any, propKey: string, descriptor: PropertyDescriptor) => {
+    restDecorHandler({
+      method: "PATCH",
+      propKey,
+      descriptor,
+      path,
+      target,
+    });
+  };
+
+export const Delete =
+  (path?: string) =>
+  (target: any, propKey: string, descriptor: PropertyDescriptor) => {
+    restDecorHandler({
+      method: "DELETE",
+      propKey,
+      descriptor,
+      path,
+      target,
+    });
+  };
